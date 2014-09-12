3a53887593f50719df1f7e98e7aa4c17df17fdf3
Johan Abbors <johan@pratsam.org>
2014-09-12 12:34:16 +0300

    Add new sample files for playfile test

--- tests/playfile.sh
+++ tests/playfile.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-${PREFIX} ${bindir:-.}/playfile ${srcdir:-.}/testdata/file1.ogg
+${PREFIX} ${bindir:-.}/playfile ${srcdir:-.}/testdata/sample.ogg
 result=$?
 test $result -eq 0 || exit $result
-${PREFIX} ${bindir:-.}/playfile ${srcdir:-.}/testdata/file2.ogg
-result=$?
-test $result -eq 0 || exit $result
-${PREFIX} ${bindir:-.}/playfile ${srcdir:-.}/testdata/file3.ogg
+${PREFIX} ${bindir:-.}/playfile ${srcdir:-.}/testdata/sample.mp3
 result=$?
 test $result -eq 0 || exit $result
